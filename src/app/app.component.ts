a85751289214ffe1ff76fef2d68e839f208a2b84
VS <sofronov.vit@gmail.com>
2024-08-04 16:19:15 +0400

    add dark theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,41 @@
-import { TuiRootModule, TuiDialogModule, TuiAlertModule } from '@taiga-ui/core';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  TuiRootModule,
+  TuiDialogModule,
+  TuiAlertModule,
+  TuiModeModule,
+  TuiThemeNightModule,
+  TuiBrightness,
+} from '@taiga-ui/core';
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { TuiThemeNightService } from '@taiga-ui/addon-doc';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TuiRootModule, TuiDialogModule, TuiAlertModule, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule,
+    HeaderComponent,
+    TuiThemeNightModule,
+    TuiModeModule,
+    AsyncPipe,
+    NgIf,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'it-summit';
+
+  constructor(@Inject(TuiThemeNightService) readonly night$: TuiThemeNightService) {}
+
+  get mode(): TuiBrightness | null {
+    return this.night$.value ? 'onDark' : null;
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { TuiLinkModule } from '@taiga-ui/core';
+import { TuiButtonModule, TuiLinkModule } from '@taiga-ui/core';
+import { TuiThemeNightService } from '@taiga-ui/addon-doc';
+import { AsyncPipe } from '@angular/common';
 
 interface NavigationItem {
   name: string;
@@ -29,11 +31,16 @@ const NAVIGATION: NavigationItem[] = [
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, TuiLinkModule],
+  imports: [RouterLink, TuiLinkModule, TuiButtonModule, AsyncPipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
   navigation = NAVIGATION;
+  constructor(@Inject(TuiThemeNightService) readonly night: TuiThemeNightService) {}
+
+  toggleDarkMode() {
+    this.night.toggle();
+  }
 }
