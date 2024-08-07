3426322da32d4acdb4b15e79126a7d71f240294c
VS <sofronov.vit@gmail.com>
2024-08-07 11:17:57 +0400

    add event details page

--- src/app/pages/event-details/event-details.component.ts
+++ src/app/pages/event-details/event-details.component.ts
@@ -1,13 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { SummitEvent } from '@core/models/models';
+import { selectSummitEventById } from '@store/summit-events/summit-events.selectors';
+import { TuiButtonModule, TuiLoaderModule } from '@taiga-ui/core';
+import { AsyncPipe, NgIf, NgOptimizedImage, NgStyle } from '@angular/common';
+import { MapComponent } from '@components/map/map.component';
+import { SummitEventsActions } from '@store/summit-events/summit-events.actions';
 
 @Component({
   selector: 'app-event-details',
   standalone: true,
-  imports: [],
+  imports: [TuiLoaderModule, NgIf, AsyncPipe, RouterLink, TuiButtonModule, NgStyle, MapComponent, NgOptimizedImage],
   templateUrl: './event-details.component.html',
   styleUrl: './event-details.component.less',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EventDetailsComponent {
+export class EventDetailsComponent implements OnInit {
+  route = inject(ActivatedRoute);
+  store = inject(Store);
 
+  eventID = this.route.snapshot.paramMap.get('id');
+  event$: Observable<SummitEvent | undefined> | null = null;
+
+  ngOnInit(): void {
+    if (this.eventID) {
+      this.event$ = this.store.select(selectSummitEventById(+this.eventID));
+    }
+    this.store.dispatch(SummitEventsActions.loadingSummitEvents());
+  }
 }
